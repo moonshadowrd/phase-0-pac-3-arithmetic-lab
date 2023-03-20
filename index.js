92793c0a253f669e0096c7ec0bc0c5f45c53c72b
jose cabreja <cabreja@yahoo.com>
2023-03-20 05:18:13 -0400

    creates myfile.md

---
+++ index.js
@@ -0,0 +1,46 @@
+function add() {
+   }
+   
+   function subtract() {
+}
+
+function multiply() {
+}
+
+function divide() {
+}
+
+function add() {
+}
+
+let num1 = 6;
+let num2 = 7;
+
+function add(num1, num2) {
+    return num1 + num2
+}
+
+function subtract( num1, num2) {
+    return num1 - num2
+}
+
+function multiply( num1, num2) {
+    return num1 * num2
+}
+
+function divide( num1, num2) {
+    return num1 / num2
+}
+
+function increment( num1) {
+    return ++num1 
+}
+
+function decrement( num1) {
+    return --num1 
+}
+
+let numer = 10;
+ parseInt("2.2222", 10);
+
+
